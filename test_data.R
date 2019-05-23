38d1a41cc239e3738c23c90e011405440ab15194
goergen95 <44396184+goergen95@users.noreply.github.com>
2019-05-23 16:07:29 +0200

    Update test_data.R

--- test_data.R
+++ test_data.R
@@ -15,8 +15,22 @@ data = read.csv("data/ESM/216_2018_1156_MOESM2_ESM.csv", sep = ",", header = T)
 
 
 plastic = data[which(data$Abbreviation %in%  c("PP","PE","PS","PA","HDPE","LDPE","PC","PES","PET","PVC")),]
-#plastic = plastic[,c(2:1863,1871)]
+plastic = plastic[,c(2:1863,1871)]
 plastic$Abbreviation = make.names(plastic$Abbreviation)
+
+source("polymeRID/createTestDataset.R")
+trainData = createTestDataset(plastic, noise = c(0,10,50,100))
+
+source("polymeRID/trainModells.R")
+
+
+
+
+
+
+
+
+
 
 spectral_data = as.matrix(plastic[,2:1864])
 wavelengths = unlist(lapply(as.list(names(plastic[2:1864])),function(x){
@@ -109,40 +123,51 @@ createTestDataset = function(data,category = "Abbreviation",noise = c(),savG = l
 
 predData = createTestDataset(plastic)
 
-trainTestDataset = function(data,category = "Abbreviation", ntree = 200, metric = "Kappa", clusterNumber = 7, levels = c("clean","noise10","noise50","noise100"),
-                            types = c("raw","norm","sg","sg.d1","sg.d2","sg.norm","sg.norm.d1","sg.norm.d2","raw.d1","raw.d2","norm.d1","norm.d2")){
-  levels = levels
-  types = types
+trainTestDataset = function(data,category = "Abbreviation",ntree=200, metric = "Kappa", clN = 7, noiseLevels = c("clean","noise10","noise50","noise100"),path=NULL,
+                            preProcTypes = c("raw",
+                                      "norm",
+                                      "sg",
+                                      "d1",
+                                      "d2",
+                                      "sg.d1",
+                                      "sg.d2",
+                                      "sg.norm",
+                                      "d1.norm",
+                                      "d2.norm",
+                                      "sg.d1.norm",
+                                      "sg.d2.norm"),...){
+  #noiseLevels = noiseLevels
+  #preProcTypes = preProcTypes
   variables = data.frame(var = 1:20,imp = 1:20)
-  for (level in 1:length(levels)){
+  for (level in 1:length(noiseLevels)){
     levelData = data[[level]]
-    for (type in 1:length(types)){
+    for (type in 1:length(preProcTypes)){
       trainingData = levelData[[type]]
       trCnt = trainCt = trainControl(method = "LOOCV", classProbs = TRUE)
       trainingData[,category] = as.factor(trainingData[,category])
       
-      cl = parallel::makeCluster(clusterNumber)
+      cl = parallel::makeCluster(clN)
       doParallel::registerDoParallel(cl)
       rfModel = train(x = trainingData[,1:ncol(trainingData)-1], y = trainingData[,category], method = "rf", trControl = trCnt, metric = metric, ntree = ntree)
       parallel::stopCluster(cl)
-      saveRDS(rfModel, file = paste0("models/rfmodel_",levels[level],"_",types[type],".rds"))
+      saveRDS(rfModel, file = paste0(path,"rfmodel_",noiseLevels[level],"_",preProcTypes[type],".rds"))
       imp = varImp(rfModel)
       
       variables$var = attributes(imp$importance)$row.names[which(imp$importance$Overall %in% sort(imp$importance$Overall, decreasing = T)[1:20])]
       variables$imp = imp$importance$Overall[which(imp$importance$Overall %in% sort(imp$importance$Overall, decreasing = T)[1:20])]
       accuracy = rfModel$results[which(rfModel$results$Kappa == max(rfModel$results$Kappa)),]
-      print(paste0("Level: ",levels[level]," Type: ",types[type]))
+      print(paste0("Level: ",noiseLevels[level]," Type: ",preProcTypes[type]))
       print(accuracy)
       predictive = rfModel$pred
       conf = rfModel$finalModel$confusion
       results = list(variables,accuracy,predictive,conf)
-      saveRDS(results,file = paste0("results/results_",levels[level],"_",types[type],".rds"))
+      saveRDS(results,file = paste0(path,"results_",noiseLevels[level],"_",preProcTypes[type],".rds"))
       
     }
   } 
 }
 
-trainTestDataset(predData)
+trainTestDataset(trainData,noiseLevels = "noise5", path = "run/")
 
 
 rfMod = readRDS("models/rfmodel_clean_norm.d1.rds")
