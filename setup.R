e3b4b59fd4a694381fc77711e2f02dc632d83af0
goergen95 <darius2402@web.de>
2019-07-18 17:04:32 +0200

    touched setup script

---
+++ setup.R
@@ -0,0 +1,31 @@
+# set root folder
+if(Sys.info()["sysname"] == "Windows"){
+  root_folder = "~/polymer"
+} else {
+  root_folder = "~/polymer"
+}
+
+# loading necessary libraries
+loadandinstall = function(mypkg) {
+  if (!is.element(mypkg, installed.packages()[,1]))
+    install.packages(mypkg)
+  library(mypkg, character.only = TRUE)}
+libs <-  c("dplyr", 
+           "reshape2", 
+           "ggplot2",
+           "magrittr",
+           "caret",
+           "keras")
+for (lib in libs){loadandinstall(lib)}
+
+# creating mandatory directories
+root= path.expand(root_folder)
+subDir= c("fun", #directory containing scripts for functions
+          "smp", #directory containing sample spectra files
+          "ref", #directory containing reference spectra files
+          "mod", #directory containing pre-trained and calibrated models
+          "doc") #directory for documentation files
+for (dir in subDir){if(!dir.exists(file.path(root, dir))){dir.create(file.path(root, dir))}}
+
+# create path variables
+for (dir in subDir){assign(dir,file.path(root, dir))}
