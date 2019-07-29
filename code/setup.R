f22f1263852a5fbe6e03a162ae804486c91acca4
goergen95 <44396184+goergen95@users.noreply.github.com>
2019-07-29 14:45:16 +0200

    adopted setup script to new directory structure

--- code/setup.R
+++ code/setup.R
@@ -10,8 +10,8 @@ loadandinstall = function(mypkg) {
   if (!is.element(mypkg, installed.packages()[,1]))
     install.packages(mypkg)
   library(mypkg, character.only = TRUE)}
-libs <-  c("dplyr", 
-           "reshape2", 
+libs <-  c("dplyr",
+           "reshape2",
            "ggplot2",
            "magrittr",
            "caret",
@@ -23,12 +23,16 @@ for (lib in libs){loadandinstall(lib)}
 
 # creating mandatory directories
 root= path.expand(root_folder)
-subDir= c("fun", #directory containing scripts for functions
+subDir= c("analysis", # directory containing rmd files which build docu website
+          "code", #directory containing scripts and functions not
+          "docs", #directory where htmls will be build
           "smp", #directory containing sample spectra files
           "ref", #directory containing reference spectra files
           "mod", #directory containing pre-trained and calibrated models
-          "doc", #directory for documentation files
-          "run") #directory for files created in runtime
+          "docs", #directory for documentation files
+          "run", #directory for files created in runtime
+          "output") # directory for additional outputs
+
 for (dir in subDir){if(!dir.exists(file.path(root, dir))){dir.create(file.path(root, dir))}}
 
 # create path variables
